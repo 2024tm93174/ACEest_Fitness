1378abf27ddfc3f99d71c486f4ceb19ab2e07d99
SILMIA <2024tm93174@wilp.bits-pilani.ac.in>
2025-09-18 18:48:28 +0300

    sonarqube code 1

--- app.py
+++ app.py
@@ -49,8 +49,7 @@ if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 
         
-""" 
-if __name__=='__main__':
+""" if __name__=='__main__':
     app = create_app()
     app.run(debug=True) """
 
